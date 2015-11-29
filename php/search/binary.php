7f2dee70560f8cfea80e2a71a901fd4f97b6195e
Jitendra <adhocore@gmail.com>
2015-11-29 12:17:43 +0545

    add search structure with binary search

---
+++ php/search/binary.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * binary_search: https://en.wikipedia.org/wiki/Binary_search_algorithm
+ *
+ * @param  array $list The ordered list of items
+ * @param  mixed $item The lookup item
+ *
+ * @return mixed       The index of item if success, null otherwise
+ */
+function binary_search(array $list, $item)
+{
+    $lo = 0;
+    $hi = count($list) - 1;
+    while ($lo <= $hi) {
+        $mid = ($lo + $hi) >> 1;
+        if ($list[$mid] === $item) {
+            return $mid;
+        }
+
+        if ($list[$mid] > $item) {
+            $hi = $mid - 1;
+        } else {
+            $lo = $mid + 1;
+        }
+    }
+}
